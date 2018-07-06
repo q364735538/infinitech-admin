38cb31a2efb11d1b02cf1ae8e2e4b5e42dc45e5d
PC-20170527PNEA\Administrator <364735538@qq.com>
2018-07-06 17:42:22 +0800

    报错了

--- src/permission.js
+++ src/permission.js
@@ -66,7 +66,7 @@ router.beforeEach((to, from, next) => {
               ]
             }
           }]
-          const modules = ['Order']
+          // const modules = ['Order']
           store.dispatch('GenerateRoutes', modules).then(() => { // 根据roles权限生成可访问的路由表
             router.addRoutes(store.getters.addRouters) // 动态添加可访问路由表
             next({ ...to, replace: true }) // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
